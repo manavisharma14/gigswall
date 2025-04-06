57e0bb680098bcaf615ab9e3b39a2ebd9dbae0e3
manavisharma14 <manavisharma14@gmail.com>
2025-04-05 20:05:49 -0500

    hi

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,8 @@ mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB connected');
+    console.log("🔌 Connecting to MongoDB at:", process.env.MONGO_URI);
+
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error(err));

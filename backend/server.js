a2bc9ab87d3cec2b05e96caff81e18533664114f
manavisharma14 <manavisharma14@gmail.com>
2025-04-05 20:07:03 -0500

    hii

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('MongoDB connected');
+    console.log('MongoDB connecteddd');
     console.log("🔌 Connecting to MongoDB at:", process.env.MONGO_URI);
 
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
